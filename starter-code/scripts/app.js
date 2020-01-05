cebe7953bcf82d3ead6ce65f9bb2e13069d68801
EmilySummers <emilysummers36@gmail.com>
2020-01-05 21:15:47 +0000

    refactored

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -3,12 +3,17 @@ function init() {
   //  DOM variables
   const playerGrid = document.querySelector('.player-grid')
   const spaces = []
-  const colors = document.querySelectorAll('img')
+  const scoreGrid = document.querySelector('.score-grid')
+  const scoreSpaces = []
+  const code = document.querySelector('.code')
+  const codeSequence = []
+  const colors = document.querySelectorAll('.color')
   let selectedColor = null
   let spacesInPlay = []
-  let resultSpacesInPlay = []
+  let scoreSpacesInPlay = []
   const submitBtn = document.querySelector('#submit')
   const newGameBtn = document.querySelector('#start-game')
+  const resetGameBtn = document.querySelector('#reset-game')
 
   //! make spaces (not in play) faded and spacesInPlay current color
   
@@ -16,9 +21,38 @@ function init() {
   // game variables
   const width = 4
   const height = 10
-  let answers = []
+  let score = []
+  const availableColors = ['green', 'purple', 'orange', 'teal', 'yellow', 'pink', 'grey', 'red']
+  const chosenColors = new Set
+  let selectedColors = []
+  let i = 0
+
+  // creates player, result and code grids
+
+  // player grid
+  Array(width * height).join('.').split('.').forEach(() => {
+    const space = document.createElement('div')
+    space.classList.add('empty-space')
+    spaces.push(space)
+    playerGrid.appendChild(space)
+  })
+
+  // result grid
+  Array(4 * height).join('.').split('.').forEach(() => {
+    const scoreSpace = document.createElement('div')
+    scoreSpace.classList.add('empty-score')
+    scoreSpaces.push(scoreSpace)
+    scoreGrid.appendChild(scoreSpace)
+  })
+
+  // code grid
+  Array(width * 1).join('.').split('.').forEach(() => {
+    const codeSpace = document.createElement('div')
+    codeSpace.classList.add('hidden-space')
+    codeSequence.push(codeSpace)
+    code.appendChild(codeSpace)
+  })
  
-  
   // functions
 
   // gets which color was clicked on and listens out of another click on spaces in play
@@ -29,131 +63,83 @@ function init() {
 
   // changes color of space in play
   function insertColor(e) {
-    if (spacesInPlay.includes(e.target)) { //! make this better?
+    if (spacesInPlay.includes(e.target)) {//! explain this
+    //! && spacesInPlay.includes(selectedColor)) //! make this work
       e.target.className = (`${selectedColor}`) // gives element only one class of color
     }
   }
-
   //! only let it add color to player space if they have not already done so elsewhere in pattern
-   
-  // generates player space grid
-  Array(width * height).join('.').split('.').forEach(() => {
-    const space = document.createElement('div')
-    space.classList.add('empty-space')
-    spaces.push(space)
-    playerGrid.appendChild(space)
-  })
-
-  // generates result grid
-  const resultGrid = document.querySelector('.result-grid')
-  const markers = []
-
-  Array(4 * height).join('.').split('.').forEach(() => {
-    const marker = document.createElement('div')
-    marker.classList.add('empty-result-space')
-    markers.push(marker)
-    resultGrid.appendChild(marker)
-  })
-
-  // generates code sequence grid
-  const code = document.querySelector('.code')
-  const codeBlock = []
-
-  Array(width * 1).join('.').split('.').forEach(() => {
-    const codeMarker = document.createElement('div')
-    codeMarker.classList.add('hidden-space')
-    codeBlock.push(codeMarker)
-    code.appendChild(codeMarker)
-  })
- 
-  // activates current spaces in play
-  let selectedColors = []
-
+  
   function playGame() {
-    playRound()
+    activateRow()
+    generateCode()
+    newGameBtn.style.display = 'none'
   }
 
-  let i = 0
-  function playRound() {
-    if (i > 39) return // stops playRound running if last round
-    let n = i + 3
+  function activateRow() {
+    if (i > 39) return // stops squresInPlay running if last round
+    const n = i + 3
     for (i; i <= n; i++) {
       spacesInPlay.push(spaces[i]) 
-      resultSpacesInPlay.push(markers[i])
-    }    
+      scoreSpacesInPlay.push(scoreSpaces[i])
+    }  
   }
-
-  // do not convert to set (just use array from the start) if duplicates are allowed
-
-  // generates random code sequence
-  const availableColors = ['green', 'purple', 'orange', 'teal', 'yellow', 'pink', 'grey', 'red']
-  const chosenColors = new Set
-
+  
   function generateCode() {
-
-    while (chosenColors.size < 4) {
+    while (chosenColors.size < 4) { //! change 4 to represent length of code
+      // do not convert to set (just use array from the start) if duplicates are allowed
       chosenColors.add(availableColors[Math.floor(Math.random() * 8)])
     }
     //!   for (let i = 0; i < 4; i++) {
     //!   chosenColors.push(availableColors[Math.floor(Math.random() * 8)])
     //!   }
-
     const colorsArray = [...chosenColors]
-    let p = 0
-    const newArray = codeBlock.map((item, p) => {
-      // item.classList.remove('hidden-space')
-      item.setAttribute('value', colorsArray[p])
-      // item.classList.add(colorsArray[p])
-      p++
-    })
-    return newArray
+    return codeSequence.map((item, p) => item.setAttribute('value', colorsArray[p]))
+    //! item.className = colorsArray[p]
   }
-  
+    
   // checks for each of the code colors - if they completely match it returns red, if they don't completely match it checks if the color is in guess code at all, returns white if yes.
-  function checkResult() {
-    for (let i = 0; i < 4; i++) { //! change 4 to represent length of codes
-      const actual = codeBlock[i].getAttribute('value')
-      const guess = spacesInPlay[i].getAttribute('class')
+  function reviewCode() {
+    spacesInPlay.forEach(spaceInPlay => selectedColors.push(spaceInPlay.getAttribute('class')))
 
-      spacesInPlay.forEach(spaceInPlay => selectedColors.push(spaceInPlay.getAttribute('class')))
-
-      if (actual === guess) {
-        console.log('Correct!')
-        answers.push('red-peg')
-      } else if (selectedColors.includes(actual)) {
-        console.log('Half right!')
-        answers.push('white-peg')
+    for (let i = 0; i < 4; i++) { //! change 4 to represent length of code
+      const codeColor = codeSequence[i].getAttribute('value')
+      const guessedColor = spacesInPlay[i].getAttribute('class')
+      if (codeColor === guessedColor) {
+        score.push('red-peg')
+      } else if (selectedColors.includes(codeColor)) {
+        score.push('white-peg')
       }
     }
-    console.log(answers)
-    markResult()
-    endResult()
+    displayScore() //! move these functions
+    endGame()
     resetRound()
-    
-    //! or use findIndex (see notes)
-    
   }
-
-  
 
   // resets arrays after round, activates next row up and restarts round
   function resetRound() {
     spacesInPlay = []
-    resultSpacesInPlay = []
+    scoreSpacesInPlay = []
     selectedColors = []
-    answers = []
-    playRound()    
+    score = []
+    activateRow()    
   }
 
-  function markResult() {
-    answers.sort()
-    for (let i = 0; i < answers.length; i++) {
-      resultSpacesInPlay[i].className = answers[i]
-    }
+  function displayScore() {
+    score.sort()
+    // for (let i = 0; i < score.length; i++) {
+    //   scoreSpacesInPlay[i].className = score[i]
+    // }
+    const newArray = scoreSpacesInPlay.map((scoreSpace, i) => { // which method??
+      while (i < score.length) {
+        return scoreSpace.className = score[i]
+      }
+    })
+    return newArray
   }
 
-  function endResult() {
-    if (answers[3] === 'red-peg') {
+  function endGame() {
+    if (score[3] === 'red-peg') {
       revealCode()
       setTimeout(function() {
         alert('You have won the game!!')
@@ -161,7 +147,7 @@ function init() {
           resetGame()
         }
       }, 500)
-    } else if (i > 39 && answers[3] !== 'red-peg') {
+    } else if (i > 39 && score[3] !== 'red-peg') {
       revealCode()
       setTimeout(function() {
         alert('Game over.')
@@ -172,39 +158,27 @@ function init() {
     }
   }
 
+  function revealCode() {
+    return codeSequence.map(peg => peg.className = peg.getAttribute('value'))
+  }
+  
   function resetGame() {
     i = 0
     chosenColors.clear()
     generateCode()
     // spacesInPlay = []
-    // resultSpacesInPlay = []
-    spaces.forEach(space => {
-      space.className = 'empty-space'
-    })
-    markers.forEach(marker => {
-      marker.className = 'empty-result-space'
-    })
-    codeBlock.forEach(codeMarker => {
-      codeMarker.className = 'hidden-space'
-    })
+    // scoreSpacesInPlay = []
+    spaces.forEach(space => space.className = 'empty-space')
+    scoreSpaces.forEach(scoreSpace => scoreSpace.className = 'empty-score')
+    codeSequence.forEach(codeSpace => codeSpace.className = 'hidden-space')
     resetRound() //! or playGame() & leave in comments above
   }
 
-  function revealCode() {
-    const newArray = codeBlock.map(item => {
-      const value = item.getAttribute('value')
-      item.classList.remove('hidden-space')
-      item.classList.add(value)
-    })
-    return newArray
-  }
-
   // event handlers
-
   colors.forEach(color => color.addEventListener('click', selectColor))
-  submitBtn.addEventListener('click', checkResult)
+  submitBtn.addEventListener('click', reviewCode)
   newGameBtn.addEventListener('click', playGame)
-  newGameBtn.addEventListener('click', generateCode)
+  resetGameBtn.addEventListener('click', resetGame)
 
 
 }
